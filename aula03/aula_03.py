48aa8d2070f316cdf8b9b7db94e8a832565faa83
PauloSantos34 <paulovasc705@outlook.com>
2025-04-10 21:20:47 -0300

    inicia o repositório

---
+++ aula03/aula_03.py
@@ -0,0 +1,5 @@
+preco = float(input('informe o preço: '))
+#desc = input('informe o desconto')
+desc = preco * (13 / 100)
+vlfinal = preco - desc
+print(vlfinal)
---
+++ aula03/exercicio1.py
@@ -0,0 +1,5 @@
+preco = 83.0
+desconto = preco * (13 / 100)
+vl_final = preco - desconto
+
+print(vl_final)
